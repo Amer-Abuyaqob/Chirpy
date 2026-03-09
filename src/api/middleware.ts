0d39b10582f077469adb64e4a3239a9201c15cdd
Amer-Abuyaqob <a.abwycwb@gmail.com>
2026-03-09 16:20:09 +0200

    Implemented `middlewareLogResponses` in `middleware.ts` to log HTTP response information. Added JSDoc for documentation.

---
+++ src/api/middleware.ts
@@ -0,0 +1,18 @@
+import type { NextFunction, Request, Response } from "express";
+
+/**
+ * Express middleware that logs HTTP response information.
+ *
+ * @param req - Incoming HTTP request.
+ * @param res - HTTP response object.
+ * @param next - Function to pass control to the next middleware.
+ * @returns void
+ */
+export function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  // TODO: Add logging logic.
+  next();
+}
