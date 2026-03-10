1299cc770d3a6f0501c619523200a0f0768f43bd
Amer-Abuyaqob <a.abwycwb@gmail.com>
2026-03-10 13:34:28 +0200

    Added `setHTMLTextUtf8Header` function in `headers.ts` to set response headers for HTML UTF-8 encoded text. Included JSDoc for documentation.

--- src/api/headers.ts
+++ src/api/headers.ts
@@ -9,3 +9,13 @@ import type { Response } from "express";
 export function setPlainTextUtf8Header(res: Response): void {
   res.set("Content-Type", "text/plain; charset=utf-8");
 }
+
+/**
+ * Sets the response headers for html UTF-8 encoded text.
+ *
+ * @param res - HTTP response whose headers will be modified.
+ * @returns void
+ */
+export function setHTMLTextUtf8Header(res: Response): void {
+  res.set("Content-Type", "text/html; charset=utf-8");
+}
